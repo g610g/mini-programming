8dab3d3dbdcda6b285f5c4ebfe0873b319f1b35f
g610g <gio.gonzales@carsu.edu.ph>
2024-04-07 00:09:22 +0800

    can read the text file

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,58 @@ pub mod utils {
     }
 }
 pub mod core {
-    use std::{collections::HashMap, error::Error, fs};
-
     use serde::{Deserialize, Serialize};
+    use std::{
+        collections::HashMap,
+        error::Error,
+        fs::{self, File},
+        io::{BufRead, BufReader},
+    };
+    #[warn(dead_code)]
     struct Syntax {}
     #[derive(Serialize, Deserialize, Debug)]
     struct Print {
         states: HashMap<String, HashMap<String, String>>,
     }
-    pub fn init() -> Result<(), Box<dyn Error>> {
+    pub fn start(filename: &str) -> Result<(), Box<dyn Error>> {
+        let print_struct: Print;
+        let mut struct_empty = true;
+        let modifed_filename = "assets/".to_string() + filename;
+        match init() {
+            Ok(p) => {
+                print_struct = p;
+                struct_empty = false;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                return Err(e);
+            }
+        }
+        if struct_empty {
+            println!("hello world");
+            return Err("THIS WILL BE HANDLED".into());
+        }
+        //modify by appending .txt into the filename passed
+        if !fs::metadata(&modifed_filename)?.is_file() {
+            return Err("file does not exist in the directory assets!".into());
+        } else {
+            //read the syntax of it
+            let reader = read_file(&modifed_filename)?;
+            for lines in reader.lines() {
+                println!("{}", lines?);
+            }
+            return Ok(());
+        }
+    }
+    fn init() -> Result<Print, Box<dyn Error>> {
         let syntax_path = "assets/syntax.json";
         let syntax_string = fs::read_to_string(syntax_path)?;
         let print: Print = serde_json::from_str(&syntax_string)?;
-        //println!("{:?}", print);
-        println!("{:?}", print.states.get("s1"));
-        Ok(())
+        Ok(print)
+    }
+    fn read_file(filename: &str) -> Result<(BufReader<File>), Box<dyn Error>> {
+        let file = File::open(filename)?;
+        let reader = BufReader::new(file);
+        Ok(reader)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
 use std::process;
 
 use mini_programming::core;
-use serde::{Deserialize, Serialize};
 use serde_json::Result;
-#[derive(Serialize, Deserialize)]
-struct Person {
-    name: String,
-    age: u8,
-    phones: Vec<String>,
-}
 fn main() {
-    match core::init() {
+    match core::start("file.txt") {
         Ok(()) => {}
         Err(e) => {
-            println!("error {}", e);
+            println!("Error: {}", e);
             process::exit(1);
         }
     }
-}
-fn parse(string_data: &str) -> Result<()> {
-    let person: Person = serde_json::from_str(string_data)?;
-    println!("name {} age {}", person.name, person.age);
-    Ok(())
 }
